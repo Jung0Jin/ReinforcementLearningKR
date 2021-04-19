610887eef5063475284bf5721ec15d160f1a1c45
Jung0Jin <qjsmdk1346@naver.com>
2021-04-20 01:30:27 +0900

    20210420

--- Page77/practice.py
+++
@@ -1,8 +0,0 @@
-import os
-
-print(__file__)
-print(os.path.realpath(__file__))
-print(os.path.abspath(__file__))
-
-import os
-print(os.listdir(os.getcwd())+"/img")
---
+++ Page96/practice.py
@@ -0,0 +1,5 @@
+lst = [[0] * 10 for _ in range(10)]
+lst[2][2] = 1
+lst[1][2] = -1
+lst[2][1] = -1
+print(lst)
